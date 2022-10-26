1267ab752c7d7c9c9af45cbf847ec1a933364a7c
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-10-27 00:28:46 +0700

    nice learn

--- src/AccountForm.tsx
+++ src/AccountForm.tsx
@@ -1,12 +1,37 @@
 import FormWrapper from "./FormWrapper";
 
-export default function AccountForm() {
+type AccountData = {
+  email: string;
+  password: string;
+};
+
+type AccountFormProps = AccountData & {
+  updateFields: (fields: Partial<AccountData>) => void;
+};
+
+export default function AccountForm({
+  email,
+  password,
+  updateFields,
+}: AccountFormProps) {
   return (
     <FormWrapper title="Account Creation">
       <label>Email</label>
-      <input autoFocus required type="email" />
+      <input
+        autoFocus
+        required
+        type="email"
+        value={email}
+        onChange={(e) => updateFields({ email: e.target.value })}
+      />
       <label>Password</label>
-      <input required min={1} type="password" />
+      <input
+        required
+        min={1}
+        type="password"
+        value={password}
+        onChange={(e) => updateFields({ password: e.target.value })}
+      />
     </FormWrapper>
   );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
+import { useState } from "react";
 import AccountForm from "./AccountForm";
 import AddressForm from "./AddressForm";
 import useMultistepForm from "./useMultistepForm";
 import UserForm from "./UserForm";
 
+type FormData = {
+  firstName: string;
+  lastName: string;
+  age: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  password: string;
+};
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+};
+
 function App() {
+  const [data, setData] = useState(INITIAL_DATA);
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
   const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
-    useMultistepForm([<UserForm />, <AddressForm />, <AccountForm />]);
+    useMultistepForm([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ]);
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    alert("Your Account Succesfully Created");
+  }
   return (
     <div
       style={{
@@ -16,9 +57,10 @@ function App() {
         margin: "1rem",
         borderRadius: ".5rem",
         fontFamily: "sans-serif",
+        maxWidth: "max-content",
       }}
     >
-      <form>
+      <form onSubmit={onSubmit}>
         <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
           {currentStepIndex + 1} / {steps.length}
         </div>
@@ -36,9 +78,7 @@ function App() {
               Back
             </button>
           )}
-          <button type="submit" onClick={next}>
-            {isLastStep ? "Finish" : "Next"}
-          </button>
+          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
         </div>
       </form>
     </div>
