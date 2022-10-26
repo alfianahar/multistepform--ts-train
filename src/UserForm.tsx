9b62c748cb9a71c102690caea0cf7a80ca915b26
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-10-26 23:35:54 +0700

    add userform

---
+++ src/UserForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function UserForm() {
+  return (
+    <>
+      <label>First Name</label>
+      <input autoFocus required type="text" />
+      <label>Last Name</label>
+      <input required type="text" />
+      <label>Age</label>
+      <input required min={1} type="number" />
+    </>
+  );
+}
